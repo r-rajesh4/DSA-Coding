3974fd285a666ccf9ddfdd890b77f017292e45ae
Rajesh Sahu <r.rajeshsahu02@gmail.com>
2024-11-12 21:32:25 +0530

    Update subtractTheProductAndSum.py
    i find the single digit number by doing  modulus. After that i add all the single digit and multiply all the single digit after that i subtract

--- Math Coding/subtractTheProductAndSum.py
+++ Math Coding/subtractTheProductAndSum.py
@@ -1,1 +1,11 @@
-
+class Solution:
+	def subtractProductAndSum(self, n: int) -> int:
+		# Your code goes here
+            sum=0
+            product=1
+            while n!=0:
+                a=n%10
+                sum +=a
+                product *=a
+                n=n//10
+            return product-sum
