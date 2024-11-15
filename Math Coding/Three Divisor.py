d47bf938984be104f73eb4282ca4a334e5645d96
Rajesh Sahu <r.rajeshsahu02@gmail.com>
2024-11-15 23:40:49 +0530

    Update Three Divisor.py

--- Math Coding/Three Divisor.py
+++ Math Coding/Three Divisor.py
@@ -1,3 +1,24 @@
+# class Solution(object):
+#     def isThree(self, n):
+#         """
+#         :type n: int
+#         :rtype: bool
+#         """
+#         count=0
+#         for i in range (1,n+1):
+#             if n%i==0:
+#                 count +=1
+#         if count ==3:
+#             return True
+#         else:
+#             return False
+        
+# s=Solution()
+# r=s.isThree(23)
+# print(r)        
+
+
+
 class Solution(object):
     def isThree(self, n):
         """
@@ -5,14 +26,17 @@ class Solution(object):
         :rtype: bool
         """
         count=0
-        for i in range (1,n+1):
+        i=1
+        for i in range (1,n,i*i):
             if n%i==0:
-                count +=1
+                count +=2
+            if i*i==n:
+                count -=1
         if count ==3:
             return True
         else:
             return False
         
 s=Solution()
-r=s.isThree(23)
+r=s.isThree(4)
 print(r)        
