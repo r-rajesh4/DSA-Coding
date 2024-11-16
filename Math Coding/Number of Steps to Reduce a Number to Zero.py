19b5da6a805ce7f59314294b77155ed07604923f
Rajesh Sahu <r.rajeshsahu02@gmail.com>
2024-11-17 00:15:19 +0530

    Add files via upload

---
+++ Math Coding/Number of Steps to Reduce a Number to Zero.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def numberOfSteps(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        step=0
+        while num!=0:
+            if num %2 ==0:
+                num //=2
+            else:
+                num -=1
+            step +=1
+        return step
+s=Solution()
+ans=s.numberOfSteps(14)
+print (ans)
