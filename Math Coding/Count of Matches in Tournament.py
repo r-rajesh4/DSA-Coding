cc3c1cb6da992014419b18f3b94c9218186151fa
Rajesh Sahu <r.rajeshsahu02@gmail.com>
2024-11-21 22:19:28 +0530

    Add files via upload

--- Math Coding/Count of Matches in Tournament.py
+++ Math Coding/Count of Matches in Tournament.py
@@ -1,5 +1,16 @@
 class Solution:
-	def numberOfMatches(self, n: int) -> int:
-		return n-1
+    def numberOfMatches(self, n: int) -> int:
+     match=0
+     while n!=1:
+        if n%2==0:
+            match +=n//2
+            n=n//2
+        else:
+            match +=(n-1)//2
+            n= (((n-1)/2)+1)
+     return round(match)
 s=Solution()
-print(s.numberOfMatches(9))
+team=int(input("Enter Number of TeAM "))
+d=s.numberOfMatches(team)
+print("Number Of matches played is ::::::",d)
+		 
