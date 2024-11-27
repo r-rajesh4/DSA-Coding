7ff609a5a06886c904a25919ffd87adfd15defa4
Rajesh Sahu <r.rajeshsahu02@gmail.com>
2024-11-27 23:55:05 +0530

    Add files via upload

--- ArrayBasics/Remove Duplicates from Sorted Array.py
+++ ArrayBasics/Remove Duplicates from Sorted Array.py
@@ -1,1 +1,10 @@
-
+class Solution:
+	def removeDuplicates(self, nums):
+		# Your code goes here
+            tnum=[]
+            for i in nums:
+                if i not in tnum:
+                    tnum.append(i)
+            return tnum
+s=Solution()
+print(s.removeDuplicates([3,3,4,]))
