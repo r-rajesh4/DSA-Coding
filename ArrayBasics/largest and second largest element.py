d4302f7fd124fdb80e1f849e97b2ad29b1acd0ce
Rajesh Sahu <r.rajeshsahu02@gmail.com>
2024-11-28 20:49:46 +0530

    Add files via upload

---
+++ ArrayBasics/largest and second largest element.py
@@ -0,0 +1,12 @@
+class Solution:
+    def largestandsecondlargest(self,arr):
+        max=0
+        second_max=0
+        for i in arr:
+            if i>max:
+                second_max=max
+                max=i
+        return max,second_max
+    
+s=Solution()
+print(s.largestandsecondlargest([2,1,4,2,6,3,7,9,15,21]))
